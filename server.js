42dc798bd54c9a38c33d201ef20426fc48396f8e
MariumIrfan2 <mariumirfan7@gmail.com>
2023-07-07 20:06:53 +0500

    update

--- server.js
+++ server.js
@@ -1,60 +1,3 @@
-// function node(name) {
-//     console.log("hello " + name)
-// }
-// node('marium')
-
-
-// const fs = require('fs');
-// const http = require('http');
-
-// const files = fs.readdir('./', (err, dir) => {
-
-//     if (err) { console.log('error', err) }
-//     else {
-//         console.log('result', dir)
-//     }
-// }
-// )
-
-// fs.readFile("./ab.txt", "utf-8", (err, file) => {
-//     console.log(file)
-// })
-
-
-// this will overwrite the text and if the file dosent exists it will create it.
-// fs.writeFile("./ab.txt", "New texttttttt from Server.js file", (err)=>{
-//     if(err){
-//         console.log(err)
-//     }else {
-//         console.log("updated")
-//     }
-// })
-
-
-// add new text after the old one appends it dosent overwrite and if file dosent 
-// exist it will create one
-// fs.appendFile("./ab.txt", " marium", (err) => {
-//     if (err) {
-//         console.log(err)
-//     } else {
-//         console.log("file appended done")
-//     }
-// })
-
-// HTTP MODULE
-
-// const Server = http.createServer((req, res) => {
-//     console.log("Server is listening on the port 5000");
-//     console.log("request URL", req.url);
-//     if (req.url == "/courses") {
-//         res.write(JSON.stringify(a));
-//         res.end()
-//     }
-// });
-
-// Server.listen(5000);
-
-// EXPRESS JS
 const express = require("express")
 const cors = require("cors")
 const mongoose = require('mongoose')
@@ -79,13 +22,15 @@ app.use("/api/institutes", InstituteRouter)
 app.use("/api/teachers", TeacherRouter)
 app.use("/api/users", UserRouter)
 
-
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+    .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
-            console.log('database connected successfully and server is listening on port 5000')
-        })
+            console.log(
+                "Database Connected Successfully and server is listening on this port 5000"
+            );
+        });
     })
     .catch((err) => {
-        console.log(err)
-    })
+        console.log(err);
+    });
